73c2dcef899592926659c5164f384825ee30779c
symonmuchemi <simonkenya846@gmail.com>
2024-09-05 07:48:56 +0300

    update: created requirements file

--- app/__init__.py
+++ app/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
+from flask.ext.sqalchemy import SQLAlchemy
 from config import env_config
 
 
+db = SQLAlchemy()
+
+
 def create_app(config:str) -> Flask:
-
     app = Flask(__name__)
-
     app.config.from_object(env_config[config])
-
     env_config[config].init_app(app)
+    
+    db.init_app(app)
 
     return app
